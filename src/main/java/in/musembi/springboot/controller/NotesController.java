7b5537433207a377d93f4e6cbd4221693e71d7c0
codeX602 <87281617+codeX602@users.noreply.github.com>
2021-11-12 00:50:38 +0300

    created new rest end point for new note

--- src/main/java/in/musembi/springboot/controller/NotesController.java
+++ src/main/java/in/musembi/springboot/controller/NotesController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,10 +28,12 @@ public class NotesController {
 	public ResponseEntity<List<Note>> readNotes(){
 		
 		return new ResponseEntity<List<Note>>(nRepo.findAll(), HttpStatus.OK);
+			
 		
-		
-		
-		
+	}
+	@PostMapping("/notes")
+	public ResponseEntity<Note> createNote(@RequestBody Note note){
+		return new ResponseEntity<Note>(nRepo.save(note), HttpStatus.CREATED);		
 	}
 
 }
--- src/main/java/in/musembi/springboot/entity/Note.java
+++ src/main/java/in/musembi/springboot/entity/Note.java
@@ -1,10 +1,17 @@
 package in.musembi.springboot.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
@@ -22,5 +29,13 @@ public class Note {
 	private String body;
 	
 	private String category;
+	
+	@Column(name="created_at", nullable=false, updatable=false)
+	@CreationTimestamp
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	@Column(name="updated_at")
+	private Date updatedAt;
 
 }
