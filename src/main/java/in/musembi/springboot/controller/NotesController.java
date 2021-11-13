11344754b847d1c843f0c54a4af21bd25ee279af
codeX602 <87281617+codeX602@users.noreply.github.com>
2021-11-14 00:13:21 +0300

    back commit

--- src/main/java/in/musembi/springboot/controller/NotesController.java
+++ src/main/java/in/musembi/springboot/controller/NotesController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +19,7 @@ import in.musembi.springboot.repository.NotesRepository;
 
 @RestController
 @RequestMapping("/api")
-
+@CrossOrigin("*")
 public class NotesController {
 	
 	
