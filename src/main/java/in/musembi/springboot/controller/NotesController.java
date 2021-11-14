445bb0ac188b5794eb61f59fa388859893e46c46
codeX602 <87281617+codeX602@users.noreply.github.com>
2021-11-15 00:28:37 +0300

    completed post mapping

--- src/main/java/in/musembi/springboot/controller/NotesController.java
+++ src/main/java/in/musembi/springboot/controller/NotesController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +37,11 @@ public class NotesController {
 	@PostMapping("/notes")
 	public ResponseEntity<Note> createNote(@RequestBody Note note){
 		return new ResponseEntity<Note>(nRepo.save(note), HttpStatus.CREATED);		
+	}
+	@GetMapping("/notes/{id}")
+	public ResponseEntity<Note> readNote (@PathVariable Long id) {
+		return new ResponseEntity<Note>(nRepo.findById(id).get(), HttpStatus.OK);
+		
 	}
 
 }
