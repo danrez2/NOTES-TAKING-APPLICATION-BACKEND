a01e8eba3224d5ef95dcfddd0ac2da5e64b57bb0
codeX602 <87281617+codeX602@users.noreply.github.com>
2021-11-16 21:35:22 +0300

    completed delete mapping

--- src/main/java/in/musembi/springboot/controller/NotesController.java
+++ src/main/java/in/musembi/springboot/controller/NotesController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,12 @@ public class NotesController {
 	@GetMapping("/notes/{id}")
 	public ResponseEntity<Note> readNote (@PathVariable Long id) {
 		return new ResponseEntity<Note>(nRepo.findById(id).get(), HttpStatus.OK);
+		
+	}
+	@DeleteMapping("/notes/{id}")
+	public ResponseEntity<HttpStatus> deleteNote(@PathVariable Long id) {
+		nRepo.deleteById(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
 		
 	}
 
