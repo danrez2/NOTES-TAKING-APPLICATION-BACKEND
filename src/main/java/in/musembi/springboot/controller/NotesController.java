1891873ac6db6860ea18f939eb99ac1400315ec5
codeX602 <87281617+codeX602@users.noreply.github.com>
2021-11-17 23:44:07 +0300

    completed the put mapping

--- src/main/java/in/musembi/springboot/controller/NotesController.java
+++ src/main/java/in/musembi/springboot/controller/NotesController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,11 @@ public class NotesController {
 	public ResponseEntity<HttpStatus> deleteNote(@PathVariable Long id) {
 		nRepo.deleteById(id);
 		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+		
+	}
+	@PutMapping("/notes")
+	public ResponseEntity<Note> updateNote(@RequestBody Note note){
+		return new ResponseEntity<Note>(nRepo.save(note), HttpStatus.OK);
 		
 	}
 
